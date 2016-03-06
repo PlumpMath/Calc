0bfe6e251b1a92958ef78d9869161ca9c3212b81
Xiaobo Zhang <zhxb515@hotmail.com>
2016-03-06 14:18:00 -0800

    add analyze_token

--- calc.py
+++ calc.py
@@ -12,10 +12,43 @@ def read_eval_print_loop():
             sys.exit(0)
         except:
             print('Unable to handle the input. Terminated.')
+            sys.exit(1)
 
 def calc_parse(line):
     """Parse a line of calculator input and return an expression tree."""
+    tokens = tokenize(line)
+    expression_tree = analyze(tokens)
+    if len(tokens) > 0:
+        raise SyntaxError('Extra token(s): ' + ' '.join(tokens))
+    return expression_tree
+
+def tokenize(line):
+    """Convert a string into a list of tokens."""
+    spaced = line.replace('(', ' ( ').replace(')', ' ) ').replace(',', ' , ')
+    return spaced.split()
+    
+def analyze(tokens):
+    """Create a tree of nested lists from a sequence of tokens."""
+    token = analyze_token(tokens.pop())
+    if type(token) in (int, float):
+        return token
+    else:
+        tokens.pop(0) # Remove (
+        return Exp(token, analyze_operands(tokens))
+
+def analyze_operands(tokens):
+    """Read a list of comma-separated operands."""
     pass
+
+def analyze_token(token):
+    """Return the value of token if it can be analyzed as a number, or token."""
+    try:
+        return int(token)
+    except (TypeError, ValueError):
+        try:
+            return float(token)
+        except (TypeError, ValueError):
+            return token
 
 def calc_eval(exp):
     """Evaluate a Calculator expression"""
